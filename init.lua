7166b0f3c61103839e3921c3a51c219394b70be0
Neal <neal.powers@outlook.com>
2025-06-10 21:12:55 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,46 @@
+require("config.lazy")
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.executable('xclip') == 1 then
+	vim.g.clipboard = {
+		name = "xclip",
+		copy = {
+			['+'] = 'xclip -selection clipboard -in',
+			['*'] = 'xclip -selection primary -in',
+		},
+		paste = {
+			['+'] = 'xclip -selection clipboard -out',
+			['*'] = 'xclip -selection primary -out',
+		},
+		cache_enabled = 1,
+	}
+end
+
+-- Numbers
+vim.opt.relativenumber = true
+
+-- Looks
+vim.opt.shiftwidth = 4
+vim.opt.termguicolors = true
+vim.cmd.colorscheme 'melange'
+
+-- LSP Keymaps
+vim.diagnostic.config({
+	virtual_text = true
+})
+
+-- Highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Controls
+vim.g.mapleader = " "
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
